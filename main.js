00f2dc65d1ebf4a6bed2264d84492abd28290a07
Natt Korat <135023751+nattkorat@users.noreply.github.com>
2024-08-08 23:45:48 +0700

    new style

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // write your code below
 for (let n = 1; n <= 100; n++) {
-  let output = "";
-  if (n % 3 == 0) output += "Fizz";
-  if (n % 5 == 0) output += "Buzz";
-  console.log(output || n);
+    if (n % 3 == 0 && n % 5 == 0){
+        console.log("FizzBuzz")
+    }else if (n % 3 == 0){
+        console.log("Fizz")
+    }else if (n % 5 == 0){
+        console.log("Buzz")
+    }else{
+        console.log(n)
+    }
 }
