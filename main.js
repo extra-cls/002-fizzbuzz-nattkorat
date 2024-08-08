c8952a77847ea659844e95e411ffb38b50e8fa24
Natt Korat <135023751+nattkorat@users.noreply.github.com>
2024-08-08 23:26:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // write your code below
+
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
